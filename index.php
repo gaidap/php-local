1b1af07b2bfa604940eea3be70967b9092772cd3
pgaida <p.gaida@omikronsystems.de>
2021-03-27 10:41:26 +0100

    played with classes

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-    $my_var1 = 'Hello  World!';
+    $my_var1 = "Hallo, Papa!";
     echo $my_var1;
+
+    class Robin {
+      function __toString () {
+        return "Was sagt Ernie?";
+      }
+    }
+
+    $my_robin = new Robin;
+    echo "<p>$my_robin</p>"
 ?>
