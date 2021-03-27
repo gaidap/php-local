2fedfd05bc961232759e4f074f06643b1ed7af3d
pgaida <p.gaida@omikronsystems.de>
2021-03-27 20:35:34 +0100

    insert something to the database

--- classes/Database.php
+++ classes/Database.php
@@ -28,4 +28,42 @@ class Database {
       $this->error = $exception->getMessage();
     }
   }
+
+  function prepareQuery($query) {
+    $this->stmt = $this->dbh->prepare($query);
+  }
+
+  private function determineParamType ($value) {
+    $type;
+    if (is_int($value)) {
+      $type = PDO::PARAM_INT;
+    } elseif (is_bool($value)) {
+      $type = PDO::PARAM_BOOL;
+    } elseif (is_string($value)) {
+      $type = PDO::PARAM_STR;
+    } else {
+      $type = PDO::PARAM_NULL;
+    }
+    return $type;
+  }
+
+  function bindQueryParam($param, $value, $type = null) {
+    if(is_null($type)) {
+      $type = $this->determineParamType($value);
+    }
+    $this->stmt->bindValue($param, $value, $type);
+  }
+
+  function excecuteQuery() {
+    return $this->stmt->execute();
+  }
+
+  function fetchLastInsertId() {
+    return $this->dbh->lastInsertId();
+  }
+
+  function resultSet() {
+    $this->excecuteQuery();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
--- index.php
+++ index.php
@@ -2,3 +2,42 @@
 require 'classes/Database.php';
 
 $db_connection = new Database();
+$post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+if($post['submitPost']) {
+  $title = $post['title'];
+  $body = $post['body'];
+  $db_connection->prepareQuery("INSERT INTO posts (title, body) VALUES(:title, :body)");
+  $db_connection->bindQueryParam(':title', $title);
+  $db_connection->bindQueryParam(':body', $body);
+  $db_connection->excecuteQuery();
+  if ($db_connection->fetchLastInsertId()) {
+    header("Location: " . $_SERVER['PHP_SELF']);
+  } else {
+    echo "Ooops. Something got wrong. Please try again.";
+  }
+}
+
+// select a specific entry
+// $db_connection->prepareQuery("select * from posts where id = :id");
+// $db_connection->bindQueryParam(':id', 1);
+$db_connection->prepareQuery("SELECT * FROM posts");
+$rows = $db_connection->resultSet();
+?>
+<h1>Create Post</h1>
+<form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+  <label>Post Title:</label><br>
+  <input type="text" name="title" placeholder="Add a title..."/><br>
+  <label>Post Message:</label><br>
+  <textarea name="body" placeholder="Add a message..."></textarea><br>
+  <input type="submit" name="submitPost" value="Submit post"/>
+</form>
+<h1>Posts</h1>
+<div>
+<?php foreach($rows as  $row): ?>
+  <div>
+    <h3><?php echo $row['title']; ?></h3>
+    <p><?php echo $row['body']; ?></p>
+  </div>
+<?php endforeach; ?>
+</div>
